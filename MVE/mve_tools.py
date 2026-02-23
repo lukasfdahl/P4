49a77b52728b59bba0a6c24c5320dcf9f1f36095
Lukasfdahl <lukas@dahlnet.dk>
2026-02-23 22:55:24 +0100

    Added optional decoding of video frames
    haven't tested due to package only having linux support

--- MVE/mve_tools.py
+++ MVE/mve_tools.py
@@ -16,32 +16,37 @@ class FrameStats(NamedTuple):
     #a class to just make video stats return more readable
 class Frame(NamedTuple):
     frame_type: str # Frametype (I = Keyframe, P = frame that only referes to past frames, B = frame that referes to both past and future frames. ? = unkown frame type)
-    motion_vector_array: ndarray
-
+    motion_vector_array: ndarray # (read here for information about what element in the array means (it is documented) https://github.com/LukasBommes/mv-extractor )
+    decoded_fame: ndarray # Only included if you pass along decode frame
 
 
 cap = VideoCap() #VideoCap is the class that has all the functions for extracting relevant information from videos
 
-# (optional) skip decoding frames
+# skip decoding frames (gets enabled if decode frame in the extract vectors function is true)
 cap.set_decode_frames(False)
 
 #url can be either a path to a file or the ip of a camera streaming with RTSP (VIDEO MUST BE H.264 encoded)
-def extract_vectors(url : str) -> list[Frame]:
+def extract_vectors(url : str, decode_frames = False) -> list[Frame]:
+    if decode_frames: #set decoding frame to true, if decoding is enabled
+        cap.set_decode_frames(True)
+    
     status = cap.open(url)
     if status == False:
         warnings.warn("Chould not open video file, or connect to cammera", UserWarning)
         cap.release()
+        cap.set_decode_frames(False)
         return [] #returning an empty list means it failed
     
     frames : list[Frame] = [] #Each element in the list is a frame, each frame is a tuple with first the type of frame as a string, follow by the motion vectors as an ndarray
 
     while True: #Loop through all frames in the video. (IMORTANT. if we need to work on streams, then impliment some system to make it not run forever)
-        success, _, motion_vectors, frame_type = cap.read()
+        success, decoded_frame, motion_vectors, frame_type = cap.read()
         if success == False: #IF the video has ended or mve fails to extract motion vectors
             break
-        frames.append(Frame(frame_type, motion_vectors))
+        frames.append(Frame(frame_type, motion_vectors, decoded_frame))
 
     cap.release()
+    cap.set_decode_frames(False)
     return frames
     
 

42537bea92018b6f40313ee9207ef9356ea49551
Lukasfdahl <lukas@dahlnet.dk>
2026-03-17 09:34:33 +0100

    added motion vector and frame data classes

---
+++ data_classes.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass
+from numpy import ndarray
+
+@dataclass
+class MotionVector: # (read here for information about what element in the array means (it is documented) https://github.com/LukasBommes/mv-extractor)
+    source : int
+    width : int
+    height : int
+    source_x : int
+    source_y : int
+    destination_x : int
+    destination_y : int
+    motion_x : int
+    motion_y : int
+    motion_scale : int
+    
+@dataclass
+class Frame:
+    motion_vectors : list[MotionVector]
+    frame_type : str # Frametype (I = Keyframe, P = frame that only referes to past frames, B = frame that referes to both past and future frames. ? = unkown frame type)
+    residuals : ndarray
+    true_bounding_boxes : list[list[float]]
+    true_classes : list[int]
